29553a8f451b02b6a7a125085be9e6054b1e9216
Olalekan <horlalekan123@gmail.com>
2022-01-22 08:29:31 +0100

    populate BrickandCondoDash.tsx with data from database

--- components/ManagePropertyCard.tsx
+++ components/ManagePropertyCard.tsx
@@ -1,55 +1,57 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
-import Link from 'next/link'
-import { DarkFullWidthCard } from './DarkFullWidthCard'
-import { LightButton } from './LightButton'
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { DarkFullWidthCard } from "./DarkFullWidthCard";
+import { LightButton } from "./LightButton";
 
-type ManagePropertyCardType = { 
-  propName: string, 
-  propTotalVisits: string,
-}
-export const ManagePropertyCard = ({ 
-  propName, 
-  propTotalVisits
+type ManagePropertyCardType = {
+  property_name: string;
+  propTotalVisits: string;
+};
+export const ManagePropertyCard = ({
+  property_name,
+  propTotalVisits,
 }: ManagePropertyCardType) => {
-  return ( 
+  return (
     <Box
-      bg='secondary.100'
-      borderRadius='xl'
+      bg="secondary.100"
+      borderRadius="xl"
       py={8}
       px={8}
-      fontFamily='ProductLight'
-      color='white'
-      w='100%'
+      fontFamily="ProductLight"
+      color="white"
+      w="100%"
     >
-      <Flex direction='column' flexWrap='wrap' gap={{lg:10}}>
+      <Flex direction="column" flexWrap="wrap" gap={{ lg: 10 }}>
         <Box>
           <Text>Property Name:</Text>
-          <Heading 
-          fontFamily='ProductBold'
-          color='white'
-          fontSize='2xl'
-          w={{lg:'80%'}}
+          <Heading
+            fontFamily="ProductBold"
+            color="white"
+            fontSize="2xl"
+            w={{ lg: "80%" }}
           >
-          {propName}
+            {property_name}
           </Heading>
         </Box>
 
         <Box>
           <Text>Property Name:</Text>
-          <Heading 
-          fontFamily='ProductBold'
-          color='white'
-          fontSize='2xl'
-          w={{lg:'80%'}}
+          <Heading
+            fontFamily="ProductBold"
+            color="white"
+            fontSize="2xl"
+            w={{ lg: "80%" }}
           >
-          {propName}
+            {property_name}
           </Heading>
         </Box>
-        
-        <Box w='fit-content'>
-          <Link href='BrickandCondoUpload'><LightButton>Manage</LightButton></Link>
+
+        <Box w="fit-content">
+          <Link href="BrickandCondoUpload">
+            <LightButton>Manage</LightButton>
+          </Link>
         </Box>
       </Flex>
     </Box>
-  )
-}
+  );
+};
--- pages/BrickandCondoDash.tsx
+++ pages/BrickandCondoDash.tsx
@@ -1,67 +1,94 @@
-import { Flex, Grid, Heading } from '@chakra-ui/react';
-import Link from 'next/link';
-import React from 'react'; 
-import { LightButton } from '../components/LightButton';
-import { LoggedInBanner } from '../components/LoggedInBanner';
-import { ManagePropertyCard } from '../components/ManagePropertyCard';
+/* eslint-disable @next/next/link-passhref */
+import { Flex, Grid, Heading } from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+import { LightButton } from "../components/LightButton";
+import { LoggedInBanner } from "../components/LoggedInBanner";
+import { ManagePropertyCard } from "../components/ManagePropertyCard";
+import { firestore } from "../firebase/config";
 
-const BrickandCondoDash = ({user}: {user: object}) => { 
-  const DeleteThisFakeData = [ 
+const BrickandCondoDash = ({ user }: { user: object }) => {
+  const [properties, setProperties] = React.useState([{}]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [hasProperty, setHasProperty] = React.useState(false);
+
+  const propertiesRef = firestore.collection("properties");
+  const getProperties = async () => {
+    setIsLoading(true);
+    propertiesRef.onSnapshot((snapShot) => {
+      if (!snapShot.empty) {
+        setHasProperty(true);
+        let loadedProperties = [];
+        for (let index = 0; index < snapShot.docs.length; index++) {
+          const data = {
+            ...snapShot.docs[index].data(),
+          };
+          loadedProperties.push(data);
+        }
+        setProperties(loadedProperties);
+      }
+    });
+    setIsLoading(false);
+  };
+  React.useEffect(() => {
+    getProperties();
+  }, [""]);
+
+  const DeleteThisFakeData = [
     {
-      propName: 'Main Street House 24 1717 Fraser Cir', 
-      propTotalVisits: '6'
+      propName: "Main Street House 24 1717 Fraser Cir",
+      propTotalVisits: "6",
     },
     {
-      propName: '1717 Fraser Cir', 
-      propTotalVisits: '0'
+      propName: "1717 Fraser Cir",
+      propTotalVisits: "0",
     },
     {
-      propName: 'Crumbs Road 13', 
-      propTotalVisits: '1'
-    }
-  ]
-  return ( 
-    <Flex direction='column'>
-      <LoggedInBanner email={user}/>
-      <Flex direction='column'>
-        <Flex align='center' mt={{lg: 10}}>
-          <Heading 
-            fontFamily='ProductBold'
-            color='secondary.100'
-            fontSize='5xl'
-            w={{lg:'80%'}}
+      propName: "Crumbs Road 13",
+      propTotalVisits: "1",
+    },
+  ];
+  return (
+    <Flex direction="column">
+      <LoggedInBanner email={user} />
+      <Flex direction="column">
+        <Flex align="center" mt={{ lg: 10 }}>
+          <Heading
+            fontFamily="ProductBold"
+            color="secondary.100"
+            fontSize="5xl"
+            w={{ lg: "80%" }}
           >
             Welcome, Admin
           </Heading>
-          <Link href='BrickandCondoUpload'><LightButton>Upload New Property</LightButton></Link>
+          <Link href="BrickandCondoUpload">
+            <LightButton>Upload New Property</LightButton>
+          </Link>
         </Flex>
-        <Flex direction='column' mt={{lg:20}}> 
-          <Heading 
-            fontFamily='ProductBold'
-            color='secondary.100'
-            fontSize='2xl'
-            w={{lg:'80%'}}
+        <Flex direction="column" mt={{ lg: 20 }}>
+          <Heading
+            fontFamily="ProductBold"
+            color="secondary.100"
+            fontSize="2xl"
+            w={{ lg: "80%" }}
           >
             Propeties to manage:
           </Heading>
-          <Flex gap={{lg:4}} mt={{lg:4}}> 
-            {
-              DeleteThisFakeData.map((item, index) => { 
-                return ( 
-                  <ManagePropertyCard 
-                    key={index}
-                    propName={item.propName}
-                    propTotalVisits={item.propTotalVisits}
-                  />
-                )
-              })
-            }
+          <Flex gap={{ lg: 4 }} mt={{ lg: 4 }}>
+            {properties.map((item: any, index) => {
+              return (
+                <ManagePropertyCard
+                  key={index}
+                  property_name={item.property_name}
+                  propTotalVisits={item.propTotalVisits}
+                />
+              );
+            })}
           </Flex>
         </Flex>
       </Flex>
-     
     </Flex>
-  )
-}
+  );
+};
 
 export default BrickandCondoDash;
