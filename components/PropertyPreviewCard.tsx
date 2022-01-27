2b1cac858c4be0e45d0029be9a2ac910c5800337
Olalekan <horlalekan123@gmail.com>
2022-01-28 00:00:53 +0100

    add getProperties funcion to home page and getProperty function to property page

--- components/PropertyPreviewCard.tsx
+++ components/PropertyPreviewCard.tsx
@@ -1,76 +1,81 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
-import BedRoomIcon from '../public/bedRoomIcon.svg';
-import SqftIcon from '../public/sqtFtIcon.svg';
-import BathRoomIcon from '../public/bathRoom.svg';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import BedRoomIcon from "../public/bedRoomIcon.svg";
+import SqftIcon from "../public/sqtFtIcon.svg";
+import BathRoomIcon from "../public/bathRoom.svg";
+import Image from "next/image";
+import Link from "next/link";
 
-
-export const PropertyPreviewCard = () => { 
-
-  return ( 
-    <Link href='/p/93937293'>
-      <Flex 
-        direction='column' 
-        pt={'30rem'}
-        bg={`linear-gradient(180.03deg, rgba(0, 0, 0, 0) 50%, #000000 99.97%), url(https://images.unsplash.com/photo-1507089947368-19c1da9775ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80)`}
-        bgPosition='center'
-        bgSize='cover'
-        pl={{base:10}}
-        pr={{lg:28, base: 20}}
-        borderRadius={{lg:'3xl', base: '2xl'}}
-        position='relative'
-        cursor='pointer'
+export const PropertyPreviewCard = ({ data }: { data: any }) => {
+  return (
+    <Link href={`/p/${data.id}`}>
+      <Flex
+        direction="column"
+        pt={"30rem"}
+        bg={`linear-gradient(180.03deg, rgba(0, 0, 0, 0) 50%, #000000 99.97%), url(${data?.images?.main})`}
+        bgPosition="center"
+        bgSize="cover"
+        pl={{ base: 10 }}
+        pr={{ lg: 28, base: 20 }}
+        borderRadius={{ lg: "3xl", base: "2xl" }}
+        position="relative"
+        cursor="pointer"
+        onClick={() => localStorage.setItem("propertyID", data.id)}
       >
-        <Flex 
-          direction='column' 
-          pb={16}
-        >
-          <Heading fontFamily='ProductBold' color='white' lineHeight='0.9' textTransform='capitalize' fontSize={{lg: '5xl', base: '4xl'}}>2420 main street hover lorad</Heading>
-          <Text fontFamily='ProductLight' color='white'>Banana Island, Off Ikre- Lagos</Text>
+        <Flex direction="column" pb={16}>
+          <Heading
+            fontFamily="ProductBold"
+            color="white"
+            lineHeight="0.9"
+            textTransform="capitalize"
+            fontSize={{ lg: "5xl", base: "4xl" }}
+          >
+            {data.property_name}
+          </Heading>
+          <Text fontFamily="ProductLight" color="white">
+            {data.property_sublocation}
+          </Text>
         </Flex>
 
-        <Flex 
-          direction='row' 
-          bg='primary.200' 
-          w={{lg: 'fit-content'}} 
-          borderRadius='2xl'
-          position='absolute'
-          py={{base: 3, lg: 6}}
-          px={{base: 4, lg: 8}}
-          align='center'
-          gap={{base: 10, lg: 10}}
-          bottom={{base: '-6%', lg: '-7%'}}
-        > 
-          <Flex 
-            direction='column' 
-          >
-            <Box w='20px' h='20px' >
-              <Image src={BedRoomIcon} />
+        <Flex
+          direction="row"
+          bg="primary.200"
+          w={{ lg: "fit-content" }}
+          borderRadius="2xl"
+          position="absolute"
+          py={{ base: 3, lg: 6 }}
+          px={{ base: 4, lg: 8 }}
+          align="center"
+          gap={{ base: 10, lg: 10 }}
+          bottom={{ base: "-6%", lg: "-7%" }}
+        >
+          <Flex direction="column">
+            <Box w="20px" h="20px">
+              <Image alt="" src={BedRoomIcon} />
             </Box>
-            <Text fontFamily='ProductLight' color='white'>2 Beds</Text>
+            <Text fontFamily="ProductLight" color="white">
+              {data.rooms} Beds
+            </Text>
           </Flex>
 
-          <Flex 
-            direction='column'
-          >
-            <Box w='20px' h='20px' >
-              <Image src={BathRoomIcon} />
+          <Flex direction="column">
+            <Box w="20px" h="20px">
+              <Image alt="" src={BathRoomIcon} />
             </Box>
-            <Text fontFamily='ProductLight' color='white'>3 Baths</Text>
+            <Text fontFamily="ProductLight" color="white">
+              {data.bathroom} Baths
+            </Text>
           </Flex>
 
-          <Flex 
-            direction='column'
-          >
-            <Box w='20px' h='20px' >
-              <Image src={SqftIcon} />
+          <Flex direction="column">
+            <Box w="20px" h="20px">
+              <Image alt="" src={SqftIcon} />
             </Box>
-            <Text fontFamily='ProductLight' color='white'>8393 Sqft.</Text>
+            <Text fontFamily="ProductLight" color="white">
+              {data.square_foot} Sqft.
+            </Text>
           </Flex>
         </Flex>
       </Flex>
     </Link>
-    
-  ) 
-}
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,211 +1,244 @@
-import { Box, Divider, Fade, Flex, Heading, Grid, Text, useDisclosure } from '@chakra-ui/react'
-import type { NextPage } from 'next'
-import Image from 'next/image'
-import searchIcon from '../public/searchIcon.svg'
-import React from 'react';
-import { PropertyPreviewCard } from '../components/PropertyPreviewCard';
+import {
+  Box,
+  Divider,
+  Fade,
+  Flex,
+  Heading,
+  Grid,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Image from "next/image";
+import searchIcon from "../public/searchIcon.svg";
+import React from "react";
+import { PropertyPreviewCard } from "../components/PropertyPreviewCard";
+import { firestore } from "../firebase/config";
 
-
-const Location = [ 
-  'Lekki', 
-  'Magodo', 
-  'Ikorodu', 
-  'Bakew', 
-  'Small London'
-];
-
-const FakeData = [ 
-  { 
-    prtyImag: 'https://images.unsplash.com/photo-1586860475241-ebd59e5bb07f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80', 
-    prtyName: '12 House Hipper', 
-    prtySublocation: 'Banana Island, Off Ikre- Lagos',
-    mainLocation: 'Magodo',
-    metaData: { 
-      bath: 2, 
-      room: 4, 
-      sqft: 1234
-    },
-    id: '3892948322432'
-  },
-  { 
-    prtyImag: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80', 
-    prtyName: '12, Grover Street Ifako', 
-    prtySublocation: 'Banana Island, Off Ikre- Lagos',
-    mainLocation: 'Lekki',
-    metaData: { 
-      bath: 5, 
-      room: 1, 
-      sqft: 9032
-    },
-    id: '09483029943'
-  },
-  { 
-    prtyImag: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80', 
-    prtyName: '12, Grover Street Ifako', 
-    prtySublocation: 'Banana Island, Off Ikre- Lagos',
-    mainLocation: 'Lekki',
-    metaData: { 
-      bath: 5, 
-      room: 1, 
-      sqft: 9032
-    },
-    id: '09483029943'
-  }, 
-  { 
-    prtyImag: 'https://images.unsplash.com/photo-1586860475241-ebd59e5bb07f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80', 
-    prtyName: '12 House Hipper', 
-    prtySublocation: 'Banana Island, Off Ikre- Lagos',
-    mainLocation: 'Magodo',
-    metaData: { 
-      bath: 2, 
-      room: 4, 
-      sqft: 1234
-    },
-    id: '3892948322432'
-  },
-]
-
-
-
+const Location = ["Lekki", "Magodo", "Ikorodu", "Bakew", "Small London"];
 
 const Home: NextPage = () => {
-  //Hooks 
-  const {isOpen, onToggle} = useDisclosure();
+  //Hooks
+  const { isOpen, onToggle } = useDisclosure();
+  const [hasProperty, setHasProperty] = React.useState(false);
+  const [propertiesRef, setPropertiesRef] = React.useState<any>(
+    firestore.collection("properties").limit(10)
+  );
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [isMoreLoading, setIsMoreLoading] = React.useState<boolean>(false);
+  const [lastDoc, setLastDoc] = React.useState<any | {}>({});
+  const [properties, setProperties] = React.useState<any | []>([]);
   //Small Components
-  const LocationItem = ({item}:{item: string}) => { 
-    return ( 
-      <Text 
-        onClick={onToggle}
-        _hover={{bg: 'secondary.100', color: 'white'}} 
-        fontFamily='ProductLight' 
-        borderRadius='xl' color='secondary.100' 
-        bg='white' 
-        p={{base: 4}}
-        transition='all 0.3s'
-        cursor='pointer'
+  const LocationItem = ({ item }: { item: string }) => {
+    const changeLocation = () => {
+      setPropertiesRef(
+        firestore
+          .collection("properties")
+          .where("property_location", "==", item)
+          .limit(10)
+      );
+      onToggle();
+    };
+    return (
+      <Text
+        onClick={changeLocation}
+        _hover={{ bg: "secondary.100", color: "white" }}
+        fontFamily="PropertyLight"
+        borderRadius="xl"
+        color="secondary.100"
+        bg="white"
+        p={{ base: 4 }}
+        transition="all 0.3s"
+        cursor="pointer"
       >
         {item}
       </Text>
-    )
-  }
+    );
+  };
 
-  const SearchComp = () => { 
-    return ( 
-      <Flex 
+  const SearchComp = () => {
+    return (
+      <Flex
         onClick={onToggle}
-        cursor='pointer'
-        borderRadius={{base: 'full', lg: 'full', md: 'xl'}} 
-        gap={{base: 4, lg: 20}} 
-        bg='white' 
-        align='center' 
-        w='fit-content' 
-        py={{lg: 4, base: 4}} 
-        px={{lg: 8, base: 4}}
-        boxShadow='0px 9px 11px 9px rgba(0, 0, 0, 0.04);'
-      > 
-        <Text fontFamily='ProductLight' color='#C5C5C5'>Filter By Location</Text>
-        <Box w={{base: '45px'}} h={{base: '45px'}}>
-          <Image src={searchIcon} />
+        cursor="pointer"
+        borderRadius={{ base: "full", lg: "full", md: "xl" }}
+        gap={{ base: 4, lg: 20 }}
+        bg="white"
+        align="center"
+        w="fit-content"
+        py={{ lg: 4, base: 4 }}
+        px={{ lg: 8, base: 4 }}
+        boxShadow="0px 9px 11px 9px rgba(0, 0, 0, 0.04);"
+      >
+        <Text fontFamily="PropertyLight" color="#C5C5C5">
+          Filter By Location
+        </Text>
+        <Box w={{ base: "45px" }} h={{ base: "45px" }}>
+          <Image alt="" src={searchIcon} />
         </Box>
       </Flex>
-    )
-  }
+    );
+  };
 
-
-  const ListLocationComp = () => { 
-    return ( 
-      <Flex 
-        direction='column' 
-        bg='primary.200' 
-        borderRadius='xl' 
-        px={{base: 4}} 
-        py={{base: 8}}
-        transition='all 4s'
-      > 
-        <Flex direction='column' gap='4'>
-          { 
-            Location.map((item, index) => {
-              return <LocationItem item={item} key={index} />
-            })
-          }
+  const ListLocationComp = () => {
+    return (
+      <Flex
+        direction="column"
+        bg="primary.200"
+        borderRadius="xl"
+        px={{ base: 4 }}
+        py={{ base: 8 }}
+        transition="all 4s"
+      >
+        <Flex direction="column" gap="4">
+          {Location.map((item, index) => {
+            return <LocationItem item={item} key={index} />;
+          })}
         </Flex>
       </Flex>
-    )
-  }
+    );
+  };
+  const getProperties = async () => {
+    setIsLoading(true);
+
+    const snapshot = await propertiesRef;
+    snapshot.onSnapshot((snapShot: any) => {
+      if (!snapShot.empty) {
+        setHasProperty(true);
+        let newProperties = [];
+
+        setLastDoc(snapShot.docs[snapShot.docs.length - 1]);
+
+        for (let i = 0; i < snapShot.docs.length; i++) {
+          newProperties.push(snapShot.docs[i].data());
+        }
+
+        setProperties(newProperties);
+      } else {
+        setLastDoc(null);
+      }
+    });
+    setIsLoading(false);
+  };
+
+  const getMore = async () => {
+    if (lastDoc) {
+      setIsMoreLoading(true);
+      let snapshot = await propertiesRef
+        .startAfter(lastDoc.data().id)
+        .limit(10);
+      snapshot.onSnapshot((snapShot: any) => {
+        if (!snapShot.empty) {
+          let newProperties = properties;
+
+          setLastDoc(snapShot.docs[snapShot.docs.length - 1]);
+
+          for (let i = 0; i < snapShot.docs.length; i++) {
+            newProperties.push(snapShot.docs[i].data());
+          }
+
+          setProperties(newProperties);
+          if (snapShot.docs.length < 10) setLastDoc(null);
+        } else {
+          setLastDoc(null);
+        }
+      });
+      setIsMoreLoading(false);
+    }
+  };
+  React.useEffect(() => {
+    getProperties();
+  }, [propertiesRef]);
 
   return (
-    <Flex direction='column' > 
-      <Flex 
-        mb={{base: 20}} 
-        direction={{base: 'column'}} 
-        position={{base: 'relative', lg: 'relative'}}
-      > 
-        <Flex 
-          direction={{base: 'column'}}
-          py={{lg:52, base: 40}}
-          px={{lg: 80, base: 10}}
-          borderRadius='2xl'
+    <Flex direction="column">
+      <Flex
+        mb={{ base: 20 }}
+        direction={{ base: "column" }}
+        position={{ base: "relative", lg: "relative" }}
+      >
+        <Flex
+          direction={{ base: "column" }}
+          py={{ lg: 52, base: 40 }}
+          px={{ lg: 80, base: 10 }}
+          borderRadius="2xl"
           bg={`linear-gradient(0deg, rgba(3, 28, 32, 0.71), rgba(3, 28, 32, 0.71)), url(https://images.unsplash.com/photo-1507089947368-19c1da9775ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80)`}
-          bgPos='center'
-          bgSize='cover'
-          fontFamily='ProductLight'
-        > 
-          <Heading mb={{base: 6}} fontSize={{base: '4xl', lg: '6xl'}} fontFamily='ProductBold' color='white'>
+          bgPos="center"
+          bgSize="cover"
+          fontFamily="ProductLight"
+        >
+          <Heading
+            mb={{ base: 6 }}
+            fontSize={{ base: "4xl", lg: "6xl" }}
+            fontFamily="PropertyBold"
+            color="white"
+          >
             Purchase your next Property-
           </Heading>
-          <Text fontFamily='ProductBold' color='white' fontSize={{base: 'sm',lg:'md'}}>
-            Brick & Condo gives you the needed <br/> assistance as your venture into real <br/> estate inveting. 
+          <Text
+            fontFamily="ProductBold"
+            color="white"
+            fontSize={{ base: "sm", lg: "md" }}
+          >
+            Brick & Condo gives you the needed <br /> assistance as your venture
+            into real <br /> estate inveting.
           </Text>
         </Flex>
 
-        <Flex 
-          direction='column'  
-          w={{base: 'fit-content'}}
-          position={{lg: 'absolute', base: 'absolute'}}
-          bottom={{base: '-10%', lg: '-7%'}}
-          left={{base: '7%', lg: '36%'}}
-          gap={{base: 4, lg: 4}}
+        <Flex
+          direction="column"
+          w={{ base: "fit-content" }}
+          position={{ lg: "absolute", base: "absolute" }}
+          bottom={{ base: "-10%", lg: "-7%" }}
+          left={{ base: "7%", lg: "36%" }}
+          gap={{ base: 4, lg: 4 }}
         >
           <SearchComp />
         </Flex>
         <Fade in={isOpen}>
-        <Flex 
-          direction='column'  
-          w={{lg: '25%', base:'70%'}}
-          position={{lg: 'absolute', base: 'absolute'}}
-          bottom={{base: '-93%', lg: '7%'}}
-          left={{base: '7%', lg: '36%'}}
-          gap={{base: 4, lg: 4}}
-          transition='display 4s'
-          zIndex='5'
-        >
-          <ListLocationComp />
-        </Flex>
+          <Flex
+            direction="column"
+            w={{ lg: "25%", base: "70%" }}
+            position={{ lg: "absolute", base: "absolute" }}
+            bottom={{ base: "-93%", lg: "7%" }}
+            left={{ base: "7%", lg: "36%" }}
+            gap={{ base: 4, lg: 4 }}
+            transition="display 4s"
+            zIndex="5"
+          >
+            <ListLocationComp />
+          </Flex>
         </Fade>
       </Flex>
 
-      <Grid 
-        mb={{lg: 40, base: 32}} 
-        rowGap={{base:'28'}}
-        columnGap={{base: '25'}}
-        direction={{base: 'column', lg: 'row'}}
-        gridTemplateColumns={{base: 'repeat(1, 1fr)', lg: 'repeat(2, 1fr)'}}
+      <Grid
+        mb={{ lg: 40, base: 32 }}
+        rowGap={{ base: "28" }}
+        columnGap={{ base: "25" }}
+        direction={{ base: "column", lg: "row" }}
+        gridTemplateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(2, 1fr)" }}
       >
-        { 
-          FakeData.map((item) => { 
-            return ( 
-              <PropertyPreviewCard />
-            )
-          })
-        }
+        {properties.map((item, index) => {
+          return <PropertyPreviewCard key={index} data={item} />;
+        })}
       </Grid>
-      <Flex direction='row' justify='center' align='center' gap={2}> 
-        <Divider  orientation='horizontal' w={{lg:'40%', base: '30%', md: '40%'}} borderColor='primary.200'/>
-        <Text cursor='pointer' fontFamily='ProductLight'>Load More</Text>
-        <Divider orientation='horizontal' w={{lg:'40%', base: '30%', md: '40%'}} borderColor='primary.200'/>
+      <Flex direction="row" justify="center" align="center" gap={2}>
+        <Divider
+          orientation="horizontal"
+          w={{ lg: "40%", base: "30%", md: "40%" }}
+          borderColor="primary.200"
+        />
+        <Text onClick={getMore} cursor="pointer" fontFamily="ProductLight">
+          Load More
+        </Text>
+        <Divider
+          orientation="horizontal"
+          w={{ lg: "40%", base: "30%", md: "40%" }}
+          borderColor="primary.200"
+        />
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
