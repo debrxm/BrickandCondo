2b4db72a3cb71ba88708375233a814ca8166fbfd
codiejay <jamesakpan016@gmail.com>
2022-01-30 14:01:18 +0100

    fixing footer and header width

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -1,4 +1,4 @@
-import { Box, Link, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react';
 import React from 'react'; 
 import Image from 'next/image';
 import LightLogo from '../public/whiteLogoBrickandCondo.png';
@@ -7,11 +7,13 @@ import DarkLogo from '../public/blackLogoBrickandCondo.png';
 
 const Header = () => { 
   return ( 
-    <Box 
-      w={{base: '90px'}}
-      mb={{lg:4}}
-    >
-      <Image src={DarkLogo} priority />
+    <Box pt={8} m='0 auto' w={{base: '95%'}}>
+      <Box 
+        w={{base: '90px'}}
+        mb={{lg:4}}
+      >
+        <Image src={DarkLogo} priority />
+      </Box>
     </Box>
   )
 }
@@ -20,35 +22,94 @@ const Footer = () => {
   return ( 
     <Box  
       mt={{lg:64, base: 32}}
-      bg='secondary.100'
       py={16}
       px={12}
       borderRadius='3xl'
+      w={{base: '95%'}}
+      mx={{base: 'auto'}}
     >
       <Box 
         w={{base: '90px'}}
         mb={14}
       >
-        <Image src={LightLogo} priority />
+        <Image src={DarkLogo} priority />
       </Box>
       <Box>
-        <Text 
-          fontFamily='ProductLight'
-          color='secondary.200'
-          fontSize={{lg:'3xl', base:'1xl'}}
-        >
-          At BrickandCondo, we are committed to helping you close on your home investment. So feel free to send us an email at <Link color='white' href='jw.org'>Hey@brickandcondo.com.</Link> <br/> <br/> We also love connecting with everyone on our page on <Link href='jw.org' color='white'>Instagram</Link> and you can checkout what we are tweeting on <Link href='jw.org' color='white'>Twitter</Link>!
+        <Flex direction={{base: 'column', lg: 'row'}} gap='10'> 
+          <Flex direction='column' gap='4'  w='100%'>
+            <Heading 
+              fontFamily='ProductBold'
+              fontSize='xl'
+              color='primary.300'
+            >
+              Explore BrickandCondo
+            </Heading>
+            <Flex gap='2' direction='column'>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Instagram</Link>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Youtube</Link>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Facebook</Link>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Twitter</Link>
+            </Flex>
+          </Flex>
 
-        </Text>
+          <Flex direction='column' gap='4'  w='100%'>
+            <Heading 
+              fontFamily='ProductBold'
+              fontSize='xl'
+              color='primary.300'
+            >
+              Talk to Us
+            </Heading>
+            <Flex gap='2' direction='column'>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Email</Link>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Office Number</Link>
+            </Flex>
+          </Flex>
+
+          <Flex direction='column' gap='4'  w='100%'>
+            <Heading 
+              fontFamily='ProductBold'
+              fontSize='xl'
+              color='primary.300'
+            >
+              Locations
+            </Heading>
+            <Flex gap='2' direction='column'>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Ikeja</Link>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Ajah</Link>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>Sangotedo</Link>
+              <Link w='fit-content' href='https://instagram.com' fontFamily='ProductLight'>VI</Link>
+            </Flex>
+          </Flex>
+
+          <Flex direction='column' gap='4' w='100%'> 
+            <Heading 
+              fontFamily='ProductBold'
+              fontSize='xl'
+              color='primary.300'
+            >
+              We are for you.
+            </Heading>
+            <Flex gap='2' direction='column'>
+              <Text fontFamily='ProductLight'>
+                Brick & Condo gives you the needed
+                assistance as your venture into real
+                estate inveting.
+              </Text>
+            </Flex>
+          </Flex>
+        </Flex>
       </Box>
     </Box>
   )
 }
 export const PageWrapper = (props:any) => { 
-  return ( 
-    <Box maxWidth={{lg: '1290px'}} mx='auto' py={{lg:10, base:12}} px={{lg:12, base: 4}}>
+  return (
+    <Box>
       <Header />
-      {props.children}
+      <Box maxWidth={{lg: '1290px'}} mx='auto' py={{lg:10, base:12}} px={{lg:12, base: 4}}>
+        {props.children}
+      </Box>
       <Footer />
     </Box>
   )
