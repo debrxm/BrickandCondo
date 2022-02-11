7d5f93b0d30ddeebc52b28af11fb03037df3883b
Ibrahim Bello <46335833+debrxm@users.noreply.github.com>
2022-02-11 14:11:08 +0100

    Add conditional reload

--- pages/p/[prptyID].tsx
+++ pages/p/[prptyID].tsx
@@ -425,7 +425,8 @@ const FullProperty = () => {
               return (
                 <PropertyPreviewCard 
                   key={index} 
-                  data={item}                
+                  data={item}
+                  shouldReload={true}
                 />
               )
             })
